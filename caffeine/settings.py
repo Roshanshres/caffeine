f3d05a281678c18fe93ed56430284fb36df11ced
roshanshres <roshanshrestha992@gamil.com>
2023-05-12 22:33:57 +0545

    tes

--- caffeine/settings.py
+++ caffeine/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+#git trst
 #test
 from pathlib import Path
 
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#test git statusgit 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
